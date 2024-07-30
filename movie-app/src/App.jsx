ac4036e034df8e21abb0eaa1cb703002a0efb287
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-30 16:07:47 +0700

    Movie List Component - Setup Và Gọi API Phim

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,12 +1,41 @@
-import './App.css';
+import { useState, useEffect } from 'react';
 import Banner from './components/Banner';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
+    const [movie, setMovie] = useState([]);
+    const [movieRate, setMovieRate] = useState([]);
+
+    useEffect(() => {
+        const fetchMovie = async () => {
+            const options = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+                },
+            };
+            const url1 = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+            const url2 = 'https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1';
+
+            const [res1, res2] = await Promise.all([fetch(url1, options), fetch(url2, options)]);
+
+            const data1 = await res1.json();
+            const data2 = await res2.json();
+
+            setMovie(data1.results);
+            setMovieRate(data2.results);
+        };
+        fetchMovie();
+    }, []);
+
     return (
-        <div>
+        <div className="bg-black">
             <Header />
             <Banner />
+            <MovieList title={'Phim Hot'} data={movie.slice(0, 5)} />
+            <MovieList title={'Phim Đề Cử'} data={movieRate.slice(0, 5)} />
         </div>
     );
 }
